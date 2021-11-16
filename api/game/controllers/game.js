f950d2a9fac59bd930c4c995da32f694e3f88f5e
Bruno <bruno-amil@hotmail.com>
2021-11-16 15:25:20 -0300

    consumindo dados da gog

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,34 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const axios = require("axios");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+  return {
+    hating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
+module.exports = {
+  populate: async (ctx) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(await getGameInfo(products[0].slug));
+  },
+};
